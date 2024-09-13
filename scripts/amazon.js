fdcd9a3e0b711f2e5c1d33821369968d5837cde5
Ashlin <ashlinpj33@gmail.com>
2024-09-13 19:18:06 +0530

    imported cart variable from card.js into amazon.js  using module

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+
 let productsHTML = "";
 products.forEach((product) => {
   productsHTML += `
@@ -80,7 +82,6 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       cartQuantity += item.quantity;
     });
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-    console.log(cartQuantity);
-    console.log(cart);
+  
   });
 });
