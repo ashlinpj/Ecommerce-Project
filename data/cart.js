4d6567cae5b5a79d6241f85c987720c93a1ba310
Ashlin <ashlinpj33@gmail.com>
2024-09-20 18:51:20 +0530

    Generate cart items with js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,35 @@
-export const cart = [];
+export const cart = [{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity:2,
+},{
+  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity:1,
+}];
+export function addTocart(button){
+    const productId = button.dataset.productId;
+      let matchingItem;
+  
+      cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = item;
+        }
+      });
+  
+      if (matchingItem) {
+        matchingItem.quantity++;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1,
+        });
+      }
+  }
+  
+  export function updateCartQuantity(){
+    let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  }
+  
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,addTocart,updateCartQuantity} from '../data/cart.js';
 import { products} from '../data/products.js';
 let productsHTML = "";
 products.forEach((product) => {
@@ -58,30 +58,12 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity++;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    addTocart(button);
+    updateCartQuantity();
+    
   
   });
 });
